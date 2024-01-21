ca0b1cbab15e446e25336073919fa26821adb505
Sathish S <s.sathishmechddesign@gmail.com>
2024-01-21 16:42:22 +0530

    feat: skills section added

--- components/skills-section.tsx
+++ components/skills-section.tsx
@@ -1,12 +1,79 @@
 import React from 'react';
 import { Heading } from './ui/heading';
+import Image from 'next/image';
 
-const SkillSection = () => {
+const SkillSection = async () => {
+	const skillData = [
+		{
+			name: 'HTML 5',
+			image:
+				'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg',
+		},
+		{
+			name: 'CSS 3',
+			image:
+				'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg',
+		},
+		{
+			name: 'React.js',
+			image:
+				'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg',
+		},
+		{
+			name: 'Next.js',
+			image:
+				'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg',
+		},
+		{
+			name: 'Typescript',
+			image:
+				'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg',
+		},
+		{
+			name: 'Tailwind css',
+			image:
+				'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg',
+		},
+		{
+			name: 'Node.js',
+			image:
+				'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg',
+		},
+		{
+			name: 'Cypress',
+			image:
+				'https://cdn.jsdelivr.net/npm/simple-icons@3.13.0/icons/cypress.svg',
+		},
+	];
 	return (
-		<div>
+		<section className='w-full py-6 md:py-24'>
 			<Heading>Skills</Heading>
-		</div>
+			<div className='grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-6 hover:cursor-pointer'>
+				{skillData.map((skill) => (
+					<SkillCard
+						key={skill.image}
+						{...skill}
+					/>
+				))}
+			</div>
+		</section>
 	);
 };
 
 export default SkillSection;
+
+function SkillCard({ name, image }: { name: string; image: string }) {
+	return (
+		<figure className='p-6 py-10 rounded-lg grid place-items-center gap-2 bg-slate-50/10 hover:scale-105 transition-all'>
+			<Image
+				src={image}
+				alt={name}
+				width={50}
+				height={50}
+			/>
+			<figcaption className='text-xs dark:text-slate-100 text-slate-700'>
+				{name}
+			</figcaption>
+		</figure>
+	);
+}
